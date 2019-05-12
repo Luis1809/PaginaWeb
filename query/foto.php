3ec11d2515f6865bfc6118726b633ad886b4fea2
P1 <wichy.97@hotmail.com>
2019-05-12 16:14:19 -0400

    Foto reload

--- query/foto.php
+++ query/foto.php
@@ -1,9 +1,9 @@
 <?php
-$directory = "C:\Users\Luis J Rivera Mendez\Desktop\Proyecto\Fotos";
-$images = glob($directory . "/*.jpg");
+  global $path;    // path to directory has to be global, using it later in html
+  $dir = "C:\Apache24\htdocs\Fotos";
+  $files = scandir($dir, 1);                // read directory backkwards
+  $picture = ($files[0]);                   // get las picture file name
+  $path = "C:\Apache24\htdocs\Fotos";
 
-foreach($images as $image)
-{
-  echo $image;
-}
+  echo ' <img width="540" height="280" class="alignnone size-large" src="../Fotos/'.$picture.'"> '
 ?>
