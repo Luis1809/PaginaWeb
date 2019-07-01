e43008daa061ed4d8ce5566a7ce4cdbd617818e5
P1 <wichy.97@hotmail.com>
2019-06-30 22:43:46 -0400

    Fotos multiples disp

--- query/foto.php
+++ query/foto.php
@@ -1,9 +1,12 @@
 <?php
+if(isset($_GET['part'])){
+  $disp= $_GET['part'];
   global $path;    // path to directory has to be global, using it later in html
-  $dir = "C:\Apache24\htdocs\Fotos";
+  $dir = "C:\Apache24\htdocs\Fotos\Micro$disp";
   $files = scandir($dir, 1);                // read directory backkwards
   $picture = ($files[0]);                   // get las picture file name
-  $path = "C:\Apache24\htdocs\Fotos";
+  $path = "C:\Apache24\htdocs\Fotos\Micro$disp";
 
-  echo ' <img width="560" height="280" class="alignnone size-large" src="../Fotos/'.$picture.'"> '
+  echo ' <img width="560" height="280" class="alignnone size-large" src="../Fotos/Micro'.$disp.'/'.$picture.'"> ';
+}
 ?>
