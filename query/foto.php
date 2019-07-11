a288bcf66cb801d66982e892cf8680d13290b2d5
luis trinidad <luistp3q06@hotmail.com>
2019-07-10 23:15:06 -0400

    conf registro

--- query/foto.php
+++ query/foto.php
@@ -2,10 +2,10 @@
 if(isset($_GET['part'])){
   $disp= $_GET['part'];
   global $path;    // path to directory has to be global, using it later in html
-  $dir = "C:\xampp\htdocs\Fotos\Micro$disp";
+  $dir = "C:/xampp/htdocs/Fotos/Micro$disp";
   $files = scandir($dir, 1);                // read directory backkwards
   $picture = ($files[0]);                   // get las picture file name
-  $path = "C:\xampp\htdocs\Fotos\Micro$disp";
+  $path = "C:/xampp/htdocs/Fotos/Micro$disp";
 
   echo ' <img width="535" height="280" class="alignnone size-large" src="../Fotos/Micro'.$disp.'/'.$picture.'"> ';
 }
